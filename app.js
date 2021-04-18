585d13e52c77b85090328f87b5c6ae0e4334980e
ariefirawan <ariefirawan@users.noreply.github.com>
2021-04-18 14:12:41 +0700

    socket.io

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
+const fs = require('fs');
+const http = require('http');
+const { Client } = require('whatsapp-web.js');
 const express = require('express');
 const dotenv = require('dotenv');
-const socketIo = require('socket.io');
+const socketIO = require('socket.io');
 const qrcode = require('qrcode');
 
+const ErrorHandler = require('./utils/ErrorHandler');
 const connectDB = require('./config/database');
-const client = require('./middlewares/auth');
-
 const replyMsg = require('./models/replyMessage');
 
-const server = express();
+const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 dotenv.config({ path: 'config/config.env' });
+
+const SESSION_FILE_PATH = './session.json';
+let sessionCfg;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+  sessionCfg = require(SESSION_FILE_PATH);
+}
+
+const client = new Client({
+  puppeteer: { headless: true },
+  session: sessionCfg,
+});
+
+client.on('authenticated', (session) => {
+  console.log('AUTHENTICATED', session);
+  sessionCfg = session;
+  fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), function (err) {
+    if (err) {
+      return next(new ErrorHandler('cannot access this resources', 401));
+    }
+  });
+});
+
+io.on('connection', (socket) => {
+  socket.emit('message', 'Connecting ....')
+});
 
 client.on('ready', () => {
   console.log('Client is Ready');
@@ -26,6 +55,7 @@ client.on('qr', (qr) => {
   qrcode.toDataURL(qr, function (err, url) {
     console.log(url);
     if (err) {
+      return next(new ErrorHandler('cannot access this resources', 401));
     }
   });
 });
@@ -36,9 +66,14 @@ client.on('message', (message) => {
   }
 });
 
+client.on('message_revoke_me', async (msg) => {
+  // Fired whenever a message is only deleted in your own view.
+  console.log(msg.body); // message before it was deleted.
+});
+
 client.initialize();
 
-server.listen(4000, () => {
+app.listen(4000, () => {
   connectDB();
   console.log(`Server running on PORT 4000`);
 });
